33c3692a7b2edd7041ff71d235546b71ee59126c
zaOum Lcx <makethembusy@proton.me>
2026-01-07 03:39:13 +0100

    replaced

--- JuceLibraryCode/BinaryData.h
+++ JuceLibraryCode/BinaryData.h
@@ -9,7 +9,7 @@
 namespace BinaryData
 {
     extern const char*   scope_png;
-    const int            scope_pngSize = 97034;
+    const int            scope_pngSize = 6243;
 
     extern const char*   TODO_md;
     const int            TODO_mdSize = 91;
